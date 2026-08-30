2210081176419f3057d530b0206ac54165c1cda6
Pedrinho <pedrohenriquen102006@gmail.com>
2026-08-29 22:10:24 -0300

    fix: correção na classe Emprestimo do arquivo classes.py

--- classes.py
+++ classes.py
@@ -12,7 +12,10 @@ class Usuario:
         self.email = email
 
 class Emprestimo:
-    def __init__(self, data_entrega, multa, disponibilidade):
+    def __init__(self, id_livro, id_usuario, data_entrega, id_emprestimo = None, multa = 0.0, disponibilidade = 1):
+        self.id_emprestimo = id_emprestimo
         self.data_entrega = data_entrega
         self.multa = multa
         self.disponibilidade = disponibilidade
+        self.id_usuario = id_usuario
+        self.id_livro = id_livro
