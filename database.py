1264ff856e5ab8f658b7cd5eeda201f61a892ea4
Pedrinho <pedrohenriquen102006@gmail.com>
2026-08-29 22:06:16 -0300

    fix: correção na tabela de usuarios

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ import sqlite3
 
 #cria uma conexão com o sqlite e cria a tabela "biblioteca" caso ela não exista
 conn = sqlite3.connect("biblioteca.db")
+#ativa as chaves estrangeiras (são desativadas por padrão) antes de abrir o banco de dados
+conn.execute("PRAGMA foreign_keys = ON;")
 
 #cria um cursor para realizar os comandos no SQL 
 cursor = conn.cursor()
@@ -16,3 +18,29 @@ cursor.execute('''
     )
 '''
 )
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS usuarios (
+    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL
+    )
+'''    
+)
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS emprestimo (
+    id_emprestimo INTEGER PRIMARY KEY,
+    id_usuario INTEGER,
+    id_livro INTEGER,
+    data_entrega TEXT,
+    multa REAL,
+    disponibilidade INTEGER DEFAULT 1 CHECK (disponibilidade IN (0,1)),
+    FOREIGN KEY (id_livro) REFERENCES livros (id_livro),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+    )
+'''
+)
+
+conn.commit()
+conn.close()
