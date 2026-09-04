91321dd74256b50f39167241fcd0654bde0e6296
Pedrinho <pedrohenriquen102006@gmail.com>
2026-09-04 01:11:33 -0300

    padronização

--- database.py
+++ database.py
@@ -1,51 +1,54 @@
 import sqlite3
 
-#cria uma conexão com o sqlite e cria a tabela "biblioteca" caso ela não exista
-conn = sqlite3.connect("biblioteca.db")
-#ativa as chaves estrangeiras (são desativadas por padrão) antes de abrir o banco de dados
-conn.execute("PRAGMA foreign_keys = ON;")
+def criar_tabelas():
+    #cria uma conexão com o sqlite e cria a tabela "biblioteca" caso ela não exista
+    conn = sqlite3.connect("biblioteca.db")
+    #ativa as chaves estrangeiras (são desativadas por padrão) antes de abrir o banco de dados
+    conn.execute("PRAGMA foreign_keys = ON;")
 
-#cria um cursor para realizar os comandos no SQL 
-cursor = conn.cursor()
+    #cria um cursor para realizar os comandos no SQL 
+    cursor = conn.cursor()
 
-#cria a tabela de livros e passa os atributos da tabela
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS livros (
-    id_livro INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo_livro TEXT NOT NULL,
-    autor TEXT NOT NULL,
-    quantidade_disponivel INTEGER DEFAULT 1 
+    #cria a tabela de livros e passa os atributos da tabela
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS livros (
+        id_livro INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo_livro TEXT NOT NULL,
+        autor TEXT NOT NULL,
+        quantidade_disponivel INTEGER DEFAULT 1 
+        )
+    '''
     )
-'''
-)
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS usuarios (
-    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT NOT NULL
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+        id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL
+        )
+    '''    
     )
-'''    
-)
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS emprestimo (
-    id_emprestimo INTEGER PRIMARY KEY,
-    id_usuario INTEGER,
-    id_livro INTEGER,
-    data_entrega TEXT,
-    multa REAL,
-    disponibilidade INTEGER DEFAULT 1 CHECK (disponibilidade IN (0,1)),
-    FOREIGN KEY (id_livro) REFERENCES livros (id_livro),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS emprestimo (
+        id_emprestimo INTEGER PRIMARY KEY,
+        id_usuario INTEGER,
+        id_livro INTEGER,
+        data_entrega TEXT,
+        multa REAL,
+        disponibilidade INTEGER DEFAULT 1 CHECK (disponibilidade IN (0,1)),
+        FOREIGN KEY (id_livro) REFERENCES livros (id_livro),
+        FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+        )
+    '''
     )
-'''
-)
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
+    return "Banco de dados e tabela criadas com sucesso!"
 
 def cadastrar_usuario(nome, email):
+
     conn = sqlite3.connect("biblioteca.db")
     cursor = conn.cursor()
 
@@ -53,26 +56,29 @@ def cadastrar_usuario(nome, email):
     INSERT INTO usuarios(
         nome, email
     ) VALUES (?, ?)
-''',
-(nome, email)
+    ''',
+    (nome, email)
     )
     conn.commit()
     conn.close()
+    return "Usuário cadastrado com sucesso!"
 
 def verificar_multa(id_usuario):
+    
     conn = sqlite3.connect("biblioteca.db")
     cursor = conn.cursor()
 
     cursor.execute('''
     SELECT SUM(multa) FROM emprestimo WHERE id_usuario = ? AND multa > 0
-''',
-(id_usuario,)
+    ''',
+    (id_usuario,)
     )
     resultado = cursor.fetchone()[0]
     conn.close
     return resultado if resultado else 0.0
 
 def emprestar_livro(id_usuario, id_livro, data_entrega):
+
     multa_pendente = verificar_multa(id_usuario)
     if multa_pendente > 0:
         return f"Não foi possivel realizar o empréstimo, o usuário possui multa de {multa_pendente:.2f} reais."
@@ -98,12 +104,14 @@ def emprestar_livro(id_usuario, id_livro, data_entrega):
 def devolver_livro(id_usuario, id_livro):
     conn = sqlite3.connect("biblioteca.db")
     cursor = conn.cursor()
-    #atualiza a tabela de livros, aumentando oa quantidade do id do livro emprestado
+
+    #Atualiza a tabela de livros, aumentando oa quantidade do id do livro emprestado
     cursor.execute('''
     UPDATE livros SET quantidade_disponivel = quantidade_disponivel + 1 WHERE id_livro = ?
     ''', (id_livro,)
     )
-    #atualiza a tabela de emprestimo, retirando o livro do usuario
+
+    #Atualiza a tabela de emprestimo, retirando o livro do usuario
     cursor.execute('''
     UPDATE emprestimo SET disponibilidade = 0 WHERE id_usuario = ? AND id_livro = ? AND disponibilidade = 1
     ''' , (id_usuario, id_livro)
