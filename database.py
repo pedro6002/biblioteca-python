91ec58a45b8e11b1afd69aa39afcca2067d4a095
Pedrinho <pedrohenriquen102006@gmail.com>
2026-09-04 02:25:56 -0300

    criação da função visualizar_usuario

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ import sqlite3
 def criar_tabelas():
     
     #Cria uma conexão com o sqlite e cria a tabela "biblioteca" caso ela não exista
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
 
     #Ativa as chaves estrangeiras (são desativadas por padrão) antes de abrir o banco de dados
-    conn.execute("PRAGMA foreign_keys = ON;")
+    conn.execute('PRAGMA foreign_keys = ON;')
 
     #Cria um cursor para realizar os comandos no SQL 
     cursor = conn.cursor()
@@ -48,37 +48,29 @@ def criar_tabelas():
 
     conn.commit()
     conn.close()
-    return "Banco de dados e tabela criadas com sucesso!"
+    return 'Banco de dados e tabela criadas com sucesso!'
 
 def cadastrar_usuario(nome, email):
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
 
-    cursor.execute('''
-    SELECT email FROM usuarios
-    WHERE email = ?
-    ''', (email,)
+    cursor.execute('SELECT email FROM usuariosWHERE email = ?', (email,)
     )
     usuario_cadastrado = cursor.fetchone()
     if usuario_cadastrado:
         conn.close()
-        return "O usuário com esse email já existe!"
+        return 'O usuário com esse email já existe!'
 
-    cursor.execute('''
-    INSERT INTO usuarios(
-        nome, email
-    ) VALUES (?, ?)
-    ''',
-    (nome, email)
+    cursor.execute('INSERT INTO usuarios(nome, email) VALUES (?, ?)',(nome, email)
     )
     conn.commit()
     conn.close()
-    return "Usuário cadastrado com sucesso!"
+    return 'Usuário cadastrado com sucesso!'
 
 def verificar_multa(id_usuario):
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -95,14 +87,14 @@ def verificar_multa(id_usuario):
 def emprestar_livro(id_usuario, id_livro):
 
     #Função para colocar data, evitando do usuário colocar data de entrega como parâmetro
-    data_entrega = (datetime.now() + timedelta(days=7)).strftime("%d-%m-%Y")
+    data_entrega = (datetime.now() + timedelta(days=7)).strftime('%d-%m-%Y')
 
     #Verifica se o usuário tem multa pendente, negando o empréstimo
     multa_pendente = verificar_multa(id_usuario)
     if multa_pendente > 0:
-        return f"Não foi possivel realizar o empréstimo, o usuário possui multa de {multa_pendente:.2f} reais."
+        return f'Não foi possivel realizar o empréstimo, o usuário possui multa de {multa_pendente:.2f} reais.'
     
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -113,18 +105,16 @@ def emprestar_livro(id_usuario, id_livro):
 
     if emprestimo_ativo:
         conn.close()
-        return "O livro já está emprestado no seu nome!"
+        return 'O livro já está emprestado no seu nome!'
 
-    cursor.execute('''
-    SELECT quantidade_disponivel FROM livros WHERE id_livro = ?
-    ''', (id_livro,)
+    cursor.execute('SELECT quantidade_disponivel FROM livros WHERE id_livro = ?', (id_livro,)
     )
     checagem = cursor.fetchone()
 
     #Checa se o livro existe e se está disponivel
     if not checagem or checagem[0] <= 0:
         conn.close()
-        return "Não foi possivel realizar o empréstimo, livro indisponível no momento."
+        return 'Não foi possivel realizar o empréstimo, livro indisponível no momento.'
 
     cursor.execute('''
     INSERT INTO emprestimo (
@@ -140,16 +130,18 @@ def emprestar_livro(id_usuario, id_livro):
     )
     conn.commit()
     conn.close()
-    return "Livro emprestado com sucesso!"
+    return 'Livro emprestado com sucesso!'
 
 def devolver_livro(id_usuario, id_livro):
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
 
     #Atualiza a tabela de livros, aumentando a quantidade do id do livro emprestado
     cursor.execute('''
-    UPDATE livros SET quantidade_disponivel = quantidade_disponivel + 1 WHERE id_livro = ?
+    UPDATE livros 
+    SET quantidade_disponivel = quantidade_disponivel + 1
+    WHERE id_livro = ?
     ''', (id_livro,)
     )
 
@@ -164,25 +156,21 @@ def devolver_livro(id_usuario, id_livro):
     )
     conn.commit()
     conn.close()
-    return "Livro devolvido com sucesso!" 
+    return 'Livro devolvido com sucesso!' 
 
 def cadastrar_livro(titulo_livro, autor):
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO livros(
-    titulo_livro, autor
-    ) VALUES (?, ?)
-    ''', (titulo_livro, autor)
+    cursor.execute('INSERT INTO livros(titulo_livro, autor) VALUES (?, ?)', (titulo_livro, autor)
     )
     conn.commit()
     conn.close()
-    return "Livro cadastrado com sucesso!"  
+    return 'Livro cadastrado com sucesso!'  
 
 def visualizar_livro():
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
 
     #Acessa os dados pelo nome das colunas
     conn.row_factory = sqlite3.Row
@@ -198,7 +186,7 @@ def visualizar_livro():
 
 def visualizar_emprestimo():
 
-    conn = sqlite3.connect("biblioteca.db")
+    conn = sqlite3.connect('biblioteca.db')
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute('''
@@ -207,3 +195,15 @@ def visualizar_emprestimo():
     visualizacao = cursor.fetchall()
     cursor.close()
     return [dict(visualizacoes) for visualizacoes in visualizacao]
+
+def visualizar_usuario():
+
+    conn = sqlite3.connect('biblioteca.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute('''
+    SELECT nome, email FROM usuarios
+    ''')
+    ver_usuarios = cursor.fetchall()
+    cursor.close()
+    return [dict(visualizar) for visualizar in ver_usuarios]
